6546adac053bb1466e008f0438d161093ac48b11
alexbsec <j.a.c.buschinelli@gmail.com>
2024-12-24 12:28:42 -0300

    removed unecessary reference to kitchen SO (only needed in container counters as they are the ones that spawn ingredients)

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 public class ClearCounter : BaseCounter
 {
     /// <summary>
-    /// Kitchen Scriptable Object to instantiate different
-    /// objects
-    /// </summary>
-    [SerializeField] private KitchenScriptableObject _kitchenSO;
-    
-    
-    /// <summary>
     /// Counter-player interaction
     /// </summary>
     public override void Interact(Player player)
     {
+        // Case when counter has object on top of it and player
+        // is not carrying
         if (HasKitchenObject() && !player.HasKitchenObject())
         {
+            // Sets the player as the object's parent 
             GetKitchenObject().SetKitchenObjectParent(player);
             return;
         }
 
+        // Case when counter does not have object on top and
+        // player is carrying it
         if (!HasKitchenObject() && player.HasKitchenObject())
         {
+            // Sets the counter as the object's parent
             player.GetKitchenObject().SetKitchenObjectParent(this);
             return;
         }
